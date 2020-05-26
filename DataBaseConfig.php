dc73e1113c1bfd42ac9dc6797c1342f4ffdbf1c6
Vishnu Sivadas <vishnusivadas.com@gmail.com>
2020-05-26 11:15:32 +0530

    Create DataBaseConfig.php

---
+++ DataBaseConfig.php
@@ -0,0 +1,21 @@
+<?php
+
+class DataBaseConfig
+{
+    public $servername;
+    public $username;
+    public $password;
+    public $databasename;
+
+    public function __construct()
+    {
+
+        $this->servername = 'localhost';
+        $this->username = 'root';
+        $this->password = '';
+        $this->databasename = 'test';
+
+    }
+}
+
+?>
